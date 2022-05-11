816568bdc688415f049a4b05cb62a3a9f82b92b0
zzr1226sdo <yc117602@hotmail.com>
2022-05-11 16:16:04 +0800

    pack js

---
+++ app/javascript/packs/message_submit.js
@@ -0,0 +1,8 @@
+document
+  .querySelector("input[name='commit']")
+  .addEventListener("click", (e) => {
+    setTimeout(() => {
+      document.querySelector("#message_content").value = "";
+      document.querySelector("input[name='commit']").disabled = false;
+    }, 100);
+  });
